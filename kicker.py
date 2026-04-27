11d47708ff2b1e78d4d782f765b52161311e05fb
Ryan Song <ryansong612@gmail.com>
2026-04-27 16:23:28 +0100

    added message intent

--- kicker.py
+++ kicker.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.voice_states = True
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
